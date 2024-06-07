a9e95f3cb468a06531393d21ed3fd5574ba95c8c
Birdy <2944006086@qq.com>
2024-06-07 23:13:46 +0800

    update

---
+++ src/Coursera/Percolation/PercolationStats.java
@@ -0,0 +1,33 @@
+//package Coursera.Percolation;
+//
+//import edu.princeton.cs.algs4.StdRandom;
+//import edu.princeton.cs.algs4.StdStats;
+//import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+//public class PercolationStats {
+//    // perform independent trials on an n-by-n grid
+//    public PercolationStats(int n, int trials) {
+//        if(n <= 0 || trials <= 0) {
+//            IllegalArgumentException;
+//        }
+//    }
+//    // sample mean of percolation threshold
+//    public double mean() {
+//
+//    }
+//    // sample standard deviation of percolation threshold
+//    public double stddev() {
+//
+//    }
+//    // low endpoint of 95% confidence interval
+//    public double confidenceLo() {
+//
+//    }
+//    // high endpoint of 95% confidence interval
+//    public double confidenceHi() {
+//
+//    }
+//    // test client (see below)
+//    public static void main(String[] args) {
+//
+//    }
+//}
