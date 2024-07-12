abbf29428b4073473d1b54a8aafd47d755c3875d
Birdy <2944006086@qq.com>
2024-07-13 00:07:08 +0800

    update

--- src/Chapter2/QuickSort.java
+++ src/Chapter2/QuickSort.java
@@ -8,18 +8,15 @@ public class QuickSort {
     private static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
-
     private static void exch(Comparable[] a, int i, int j) {
         Comparable t = a[i];
         a[i] = a[j];
         a[j] = t;
     }
-
     public static void sort(Comparable[] a) {
         StdRandom.shuffle(a);
         sort(a, 0, a.length - 1);
     }
-
     private static void sort(Comparable[] a, int lo, int hi) {
         if(hi <= lo)
             return ;
@@ -27,7 +24,6 @@ public class QuickSort {
         sort(a, lo, j - 1);
         sort(a, j + 1, hi);
     }
-
     private static int partition(Comparable[] a, int lo, int hi) {
         int i = lo, j = hi + 1;
         Comparable v = a[lo];
